906517bb606dc0d3d2f5051fb33ef4eeabbbaa44
Brian Sipple <brsipple42@gmail.com>
2017-05-08 01:09:42 -0700

    minor polish for dummy app demo

---
+++ tests/dummy/app/components/demo-page-divider.js
@@ -0,0 +1,8 @@
+import DividerDotsComponent from 'ember-divider-dots/components/divider-dots';
+
+export default DividerDotsComponent.extend({
+  classNames: ['c-demo-page-divider'],
+
+  numDots: 8,
+  gapSizePct: 300
+});
